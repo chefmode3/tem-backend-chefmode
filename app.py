cad03a80085f4bb559fbb7f858c3555491063e76
haiderjaved4 <haiderjavaid.dev@gmail.com>
2024-10-30 15:10:13 +0500

    ssl cert non.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from os.path import join, dirname
 from dotenv import load_dotenv
 import logging
-
+import ssl
 from extractors import fetch_description
 
 dotenv_path = join(dirname(__file__), '.env')
@@ -12,16 +12,20 @@ from flask_cors import CORS
 
 from celery import Celery
 
-
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 celery = Celery(
     __name__,
-    broker="redis://:p001004ca65035c7d381457ecf466defc3710bc746fcca3d97b41b0184759034c@ec2-18-206-109-119.compute-1.amazonaws.com:7830",
-    backend="redis://:p001004ca65035c7d381457ecf466defc3710bc746fcca3d97b41b0184759034c@ec2-18-206-109-119.compute-1.amazonaws.com:7830"
+    broker="rediss://:p001004ca65035c7d381457ecf466defc3710bc746fcca3d97b41b0184759034c@ec2-18-206-109-119.compute-1.amazonaws.com:7830",
+    backend="rediss://:p001004ca65035c7d381457ecf466defc3710bc746fcca3d97b41b0184759034c@ec2-18-206-109-119.compute-1.amazonaws.com:7830",
+    broker_use_ssl={
+        'ssl_cert_reqs': ssl.CERT_NONE
+    },
+    redis_backend_use_ssl={
+        'ssl_cert_reqs': ssl.CERT_NONE
+    }
 )
-
 
 logging.getLogger('flask_cors').level = logging.DEBUG
 
@@ -30,12 +34,14 @@ logging.getLogger('flask_cors').level = logging.DEBUG
 def call_fetch_description(self, data):
     return fetch_description(data)
 
+
 @app.route("/get/recipe", methods=['POST'])
 def image_extractor():
     data = request.json
     task = call_fetch_description.delay(data)
 
     return task.task_id
+
 
 @app.route("/fetch_results", methods=['GET'])
 def fetch_results():
