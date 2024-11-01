3c1b699abc13f6d6fe2d09410601d479105ab12c
haiderjaved4 <haiderjavaid.dev@gmail.com>
2024-11-01 17:17:23 +0500

    updated redis_udl

--- app.py
+++ app.py
@@ -25,6 +25,12 @@ def create_app():
         __name__,
         broker="rediss://:p001004ca65035c7d381457ecf466defc3710bc746fcca3d97b41b0184759034c@ec2-107-22-116-4.compute-1.amazonaws.com:2977",
         backend="rediss://:p001004ca65035c7d381457ecf466defc3710bc746fcca3d97b41b0184759034c@ec2-107-22-116-4.compute-1.amazonaws.com:29779",
+        broker_use_ssl={
+            'ssl_cert_reqs': ssl.CERT_NONE
+        },
+        redis_backend_use_ssl={
+            'ssl_cert_reqs': ssl.CERT_NONE
+        }
 
     )
 
