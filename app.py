67101b4d38413488f2e644d489ee183d07e9b921
haiderjaved4 <haiderjavaid.dev@gmail.com>
2024-11-01 18:07:07 +0500

    updated redis_udl

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def create_app():
 
     celery = Celery(
         __name__,
-        broker="rediss://:p001004ca65035c7d381457ecf466defc3710bc746fcca3d97b41b0184759034c@ec2-107-22-116-4.compute-1.amazonaws.com:2977",
-        backend="rediss://:p001004ca65035c7d381457ecf466defc3710bc746fcca3d97b41b0184759034c@ec2-107-22-116-4.compute-1.amazonaws.com:29779",
+        broker="rediss://:p001004ca65035c7d381457ecf466defc3710bc746fcca3d97b41b0184759034c@ec2-107-22-116-4.compute-1.amazonaws.com:29779/0",
+        backend="rediss://:p001004ca65035c7d381457ecf466defc3710bc746fcca3d97b41b0184759034c@ec2-107-22-116-4.compute-1.amazonaws.com:29779/0",
         broker_use_ssl={
             'ssl_cert_reqs': ssl.CERT_NONE
         },
