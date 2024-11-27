122c674ebe3c6a21e777443a0746de73ec599f63
noutcheu libert <libert.noutcheu@gmail.com>
2024-11-27 11:42:52 +0100

    replace user dict by the model

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -63,10 +63,10 @@ class SignupResource(Resource):
 
             if is_activate:
                 return {"result": "Account created"}, 200
-            if user_data.reste_token:
+            if user_data.reset_token:
                 return {"result": "An email has already send please check your email to verify your address"}, 200
-            email = user_data.get("email")
-            name = user_data.get("name")
+            email = user_data.emai
+            name = user_data.name
             subject = "Email Activation"
             # email, body, subject, recipient
             url_frontend = os.getenv('VERIFY_EMAIL')
