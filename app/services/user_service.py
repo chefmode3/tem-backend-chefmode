a5fb21684f7970b445f24427f0a235f894dc715b
noutcheu libert <libert.noutcheu@gmail.com>
2024-11-27 11:14:57 +0100

    add mising migration file 030c72a64532_.py

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,7 +1,6 @@
 import logging
 
 from flask import abort
-from flask_jwt_extended import create_access_token
 from flask_login import login_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
