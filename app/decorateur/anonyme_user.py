cd64da0fc88ebbeff34d7af31ab9064fa7d99fbf
noutcheu libert <libert.noutcheu@gmail.com>
2024-11-27 16:56:12 +0100

    add custom header

--- app/decorateur/anonyme_user.py
+++ app/decorateur/anonyme_user.py
@@ -1,8 +1,12 @@
-import logging
-from flask import session, request, jsonify, g
+from __future__ import annotations
 
+import logging
 from functools import wraps
-from flask import jsonify, make_response
+
+from flask import g
+from flask import jsonify
+from flask import make_response
+from flask import request
 
 from app.services import AnonymeUserService
 
@@ -15,13 +19,13 @@ def load_or_create_anonymous_user(f):
     def decorated_function(*args, **kwargs):
         try:
             # Check if the user is logged in
-            if g.get("user", None):
+            if g.get('user', None):
                 return f(*args, **kwargs)
 
             # Get the client UUID
-            client_uuid = request.headers.get("X-Client-UUID")
+            client_uuid = request.headers.get('X-Client-UUID')
             anonymous_user = None
-            new_user_created = False
+            #  new_user_created = False
             Logger.error(f"anonyme user {client_uuid}")
             if client_uuid:
                 # Try to fetch the anonymous user
@@ -30,7 +34,6 @@ def load_or_create_anonymous_user(f):
             if not anonymous_user:
                 # Create a new anonymous user if not found
                 anonymous_user = AnonymeUserService.create_anonymous_user()
-                new_user_created = True
 
             # Link the anonymous user to the global context
             g.user = anonymous_user
@@ -52,12 +55,12 @@ def load_or_create_anonymous_user(f):
 
             # Modify headers if a new user was created
 
-            response_obj.headers["X-Client-UUID"] = anonymous_user.id
-
+            response_obj.headers['X-Client-UUID'] = anonymous_user.id
+            response_obj.headers['Access-Control-Expose-Headers'] = 'X-Client-UUID'
             return response_obj
         except Exception as err:
             Logger.error(f"Error in load_or_create_anonymous_user: {str(err)}")
-            return {"error": "An unexpected error occurred"}, 400
+            return {'error': 'An unexpected error occurred'}, 400
 
     return decorated_function
 
@@ -67,16 +70,16 @@ def track_anonymous_requests(f):
     def decorated_function(*args, **kwargs):
         try:
             # Retrieve the user from the global context
-            user = g.get("user", None)
+            user = g.get('user', None)
 
             # Ensure the user exists and has a request_count attribute
             if not user:
-                Logger.error("No user found in global context")
+                Logger.error('No user found in global context')
                 return f(*args, **kwargs)
 
             # Ensure the user has a request_count
             if not hasattr(user, 'request_count') or user.request_count is None:
-                Logger.error("User has no request_count attribute or it is None")
+                Logger.error('User has no request_count attribute or it is None')
                 return f(*args, **kwargs)
 
             # Increment the request count
@@ -86,14 +89,12 @@ def track_anonymous_requests(f):
             # Check if the request limit is exceeded
             if updated_user.request_count >= 6:
                 Logger.warning(f"Request limit reached for user {user.id}")
-                return {"error": "Maximum free requests reached. Please register."}, 403
+                return {'error': 'Maximum free requests reached. Please register.'}, 403
 
             return f(*args, **kwargs)
 
         except Exception as err:
             Logger.error(f"Error in track_anonymous_requests: {err}")
-            return {"error": "Internal server error"}, 500
+            return {'error': 'Internal server error'}, 500
 
     return decorated_function
-
-
