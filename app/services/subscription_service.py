209c631b636b740b5b5e4a1562dc5a45c498b287
tchassijordan <tchassijordan@gmail.com>
2024-12-07 18:16:06 +0100

    fix: correctly infer stripe user price id

--- app/services/subscription_service.py
+++ app/services/subscription_service.py
@@ -171,7 +171,7 @@ class SubscriptionWebhookService:
         elif self.event_type == "invoice.payment_succeeded":
             s_membership = SubscriptionMembership.query.filter_by(customer_id=customer_id).first()
             if not s_membership and stripe_user:
-                subscription = Subscription.query.filter_by(price_id=price_id).first()
+                subscription = Subscription.query.filter_by(price_id=stripe_user.price_id).first()
                 if not subscription:
                     raise SubscriptionException("Internal Server Error", 500)
                 s_membership = SubscriptionMembership(
